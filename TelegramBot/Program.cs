76dbfcc0b83bce6e42ee29b1029eb8b869d744bd
Vadim <vadim_chudinovich@mail.ru>
2021-09-22 16:06:25 +0300

    add metadata to audio file

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -155,7 +155,8 @@ namespace TelegramBot
                     await client.SendTextMessageAsync(msg.Chat.Id,
                         "Привет");
                     await client.SendAudioAsync(msg.Chat.Id,
-                        "https://github.com/Chudovad/TelegramBot/blob/master/TelegramBot/files/audio_hello.mp3");
+                        "https://github.com/Chudovad/TelegramBot/raw/master/TelegramBot/files/audio_hello.mp3");
+                    // работает https://github.com/TelegramBots/book/raw/master/src/docs/audio-guitar.mp3
                 }
                 else if (msg.Text[0] == '/')
                 {
